a3c625e95d44d5fa4998f94d4d3d972ef75be1b9
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2025-08-13 08:46:20 +0100

    go

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,7 +1,5 @@
 import systemMiddleware from './system.middleware';
-import authMiddleware from './auth.middleware';
 import authorizationMiddleware from './authorization.middleware';
 
 export { systemMiddleware };
-export { authMiddleware };
 export { authorizationMiddleware };
