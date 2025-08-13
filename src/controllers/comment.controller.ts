24c2a040785c0cbce05f01f02dcc331366d9b623
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2025-08-13 12:42:00 +0100

    go

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -78,18 +78,6 @@ class CommentController {
       res.status(404).json({ error: 'No post with this user id found' });
     }
   }
-
-  public async uploadFile(req: Request, res: Response, next: NextFunction) {
-    try {
-      // console.log('req body name', req.body.name);
-      console.log('req file', req.file);
-      res.json({ message: 'Successfully uploaded files' });
-    } catch (error) {
-      console.error(`Error retrieving post: ${error}`);
-      res.status(404).json({ error: 'No post with this user id found' });
-    }
-  }
-
   /**
    * Update a post by ID
    * @param req - Express request object
