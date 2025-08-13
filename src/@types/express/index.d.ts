eae015ca19dd3981fe73dd6ef3889586a78d881a
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2025-08-13 12:34:21 +0100

    go

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,12 +1,9 @@
 import { QueryOptions } from '../../interface/functions.interface';
 import User from '../../db/models/user.model';
-import Multer from 'multer';
 
 declare module 'express' {
   export interface Request {
     queryOpts?: QueryOptions;
-    file?: Multer.File;
-    files?: Multer.File[];
     user?: User;
     paramIds?: {
       [key: string]: number;
